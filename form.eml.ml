983dfbeace4179d633826d1828e623c0a11cbce8
A.J. Stein <alexander.stein@nist.gov>
2023-07-05 16:41:32 -0400

    Attempt 3: Example D with stateful forms.

---
+++ form.eml.ml
@@ -0,0 +1,37 @@
+let show_form ?message request =
+  <html>
+  <body>
+
+%   begin match message with
+%   | None -> ()
+%   | Some message ->
+      <p>You entered: <b><%s message %>!</b></p>
+%   end;
+
+    <form method="POST" action="/">
+      <%s! Dream.csrf_tag request %>
+      <input name="message" autofocus>
+    </form>
+
+  </body>
+  </html>
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.memory_sessions
+  @@ Dream.router [
+
+    Dream.get  "/"
+      (fun request ->
+        Dream.html (show_form request));
+
+    Dream.post "/"
+      (fun request ->
+        match%lwt Dream.form request with
+        | `Ok ["message", message] ->
+          Dream.html (show_form ~message request)
+        | _ ->
+          Dream.empty `Bad_Request);
+
+  ]
